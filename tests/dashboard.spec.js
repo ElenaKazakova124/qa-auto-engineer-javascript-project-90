1a2bb697d4e26211f3a638624b22a01a7a194ab5
Elena <elena.kazakova124@gmail.com>
2025-11-13 17:00:02 +0300

    fix 2

--- tests/dashboard.spec.js
+++ tests/dashboard.spec.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test'
-import LoginPage from '../pages/LoginPage.js'
+import LoginPage from '/pages/LoginPage.js'
 import DashboardPage from './pages/DashboardPage.js'
 import StatusesPage from './pages/StatusesPage.js'
 import LabelsPage from './pages/LabelsPage.js'
