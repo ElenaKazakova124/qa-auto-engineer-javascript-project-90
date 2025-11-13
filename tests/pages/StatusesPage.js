7a2899127073359f73af32fa843e0806e298dc7c
Elena <elena.kazakova124@gmail.com>
2025-11-13 17:46:49 +0300

    fix 6

--- tests/pages/StatusesPage.js
+++ tests/pages/StatusesPage.js
@@ -1,4 +1,4 @@
-import { BasePage } from './BasePage.js'
+import BasePage from './BasePage.js'
 
 class StatusesPage extends BasePage {
   constructor(page) {
--- tests/pages/TasksPage.js
+++ tests/pages/TasksPage.js
@@ -1,4 +1,4 @@
-import { BasePage } from './BasePage.js'
+import BasePage from './BasePage.js'
 
 class TasksPage extends BasePage {
   constructor(page) {
