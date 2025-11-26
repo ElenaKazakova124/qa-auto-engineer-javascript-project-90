11387135f28972e9fe59416623abca0f3826f023
Elena <elena.kazakova124@gmail.com>
2025-11-26 15:32:52 +0300

    fix playwright 6

--- playwright.config.js
+++ playwright.config.js
@@ -54,4 +54,12 @@ export default defineConfig({
       use: { ...devices['Desktop Safari'] },
     },
   ],
+
+  /* Launch development server during tests */
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
 });
