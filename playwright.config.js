5c6d868f442b6293d4cd35af927546791f5b7190
Elena <elena.kazakova124@gmail.com>
2025-12-03 16:15:18 +0300

    fix playwright 11

--- playwright.config.js
+++ playwright.config.js
@@ -2,14 +2,6 @@
 import { defineConfig, devices } from '@playwright/test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
@@ -56,7 +48,12 @@ export default defineConfig({
   ],
 
   /* Launch development server during tests */
-  webServer: {
+  webServer: process.env.CI ? {
+    command: 'npm run build && npm run serve',
+    url: 'http://localhost:5173',
+    reuseExistingServer: false,
+    timeout: 180 * 1000,
+  } : {
     command: 'npm run dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
